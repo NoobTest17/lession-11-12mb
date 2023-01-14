48976eb3478e6b34e51d0840467866e34d8f8688
NoobTest17 <maximnnn.nnn@yandex.ru>
2023-01-14 18:15:21 +0300

    init

---
+++ dz.js
@@ -0,0 +1,37 @@
+class People {
+  constructor({name, surname, peopleIsLove}) {
+    this.name = name;
+    this.surname = surname;
+    this.peopleIsLove = peopleIsLove;
+  }
+
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+}
+
+class Worker extends People {
+  constructor(props, rete, day) {
+    super(props);
+    this.rete = rete;
+    this.day = day;
+  }
+
+  getSalary() {
+    return this.day * this.rete
+  }
+}
+
+const Worker1 = new Worker({
+  name: 'Max',
+  surname: 'Mig',
+  peopleIsLove: 'NikaWah'
+}, 140, 20);
+
+const Worker2 = new Worker({
+  name: 'Nika',
+  surname: 'Wah',
+  peopleIsLove: 'MaxMig'
+}, 240, 28)
+
+console.log(Worker1.getFullName())
